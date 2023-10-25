81072c04c5c6924cd71594c70f7778bc33461b59
Alexuber <alexuberbuber@gmail.com>
2023-10-25 22:38:14 +0300

    finished task 3

---
+++ 03_cli_telegram_console_sender/app.js
@@ -0,0 +1,33 @@
+const TelegramBot = require("node-telegram-bot-api");
+const program = require("commander");
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const CHAT_ID = Number(process.env.CHAT_ID);
+
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, {
+  polling: true,
+});
+
+program
+  .command("send-message <message>")
+  .description("send message to Telegram bot")
+  .action((message) => {
+    bot
+      .sendMessage(CHAT_ID, message)
+      .then(() => process.exit(0))
+      .catch((error) => console.log("Error sending your message:", error));
+  });
+
+program
+  .command("send-photo <path>")
+  .description(
+    "send photo to Telegram bot. Just drag and drop it console after p-flag"
+  )
+  .action((path) => {
+    bot
+      .sendPhoto(CHAT_ID, path)
+      .then(() => process.exit(0))
+      .catch((error) => console.log("Error sending your photo:", error));
+  });
+
+program.parse(process.argv);
